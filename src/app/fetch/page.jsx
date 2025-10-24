0786350fa82e1a14a3f0672a4bbccb85fc0465f7
shifyannn <asifyan01@gmail.com>
2025-10-24 16:07:20 +0700

    test

--- src/app/fetch/page.jsx
+++ src/app/fetch/page.jsx
@@ -4,13 +4,14 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Card, CardContent } from "@/components/ui/card";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 export default function FetchData() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await axios.get("https://pokeapi.co/api/v2/ability");
+        const data = await axios.get("https://pokeapi.co/api/v2/pokemon/");
         setData(data.data.results);
       } catch (err) {
         console.log(err);
@@ -25,12 +26,18 @@ export default function FetchData() {
         <Navbar></Navbar>
       </div>
       <div className="flex justify-center items-center py-20">
-        <Card className="w-[500px]">
-          <CardContent>
+        <Card className="w-[500px] py-4 px-4 ">
+          <CardContent className="flex flex-col gap-4">
             {data.map((e, i) => {
               return (
-                <div key={i}>
+                <div
+                  key={i}
+                  className="flex justify-between  border-b border-b-gray-600"
+                >
                   <h1>{e.name}</h1>
+                  <Link target="blank" href={e.url}>
+                    Link
+                  </Link>
                 </div>
               );
             })}
